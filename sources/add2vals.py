0fdc855bdccc7158731d516fb93128b7da41dc77
Giles Gaskell <gilesgas@gmail.com>
2017-12-06 14:57:56 +1100

    Amend/fix output for 'add2values' when function is not used correctly

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -16,8 +16,10 @@ if argnumbers == 2 :
 
 if argnumbers != 2 :
     print("")
-    print("Usage: add X Y, where X and Y are individual values.")
-    print("       If uncompiled, usage is 'python add2vals.py X Y'.")
-    print("       (You entered " + str(argnumbers) + " value/s.)")
+    print("You entered " + str(argnumbers) + " value/s.")
+    print("")
+    print("Usage: 'add2vals X Y' where X and Y are individual values.")
+    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
+    print("       If unbundled, usage is 'python add2vals.py X Y'.")
     print("")
     sys.exit(1)
