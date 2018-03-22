e02ea3fd3f8f58931e54f4d232a721dc89f91c7a
kudlaty <mgolaszewski@virtuslab.com>
2018-03-22 18:51:09 +0100

    feature/kjug artifacts added

--- sources/test_webapp.py
+++ sources/test_webapp.py
@@ -8,7 +8,7 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!kdsjflsd')
+        self.assertEqual(response.data, b'Hello, World!')
 
 
 if __name__ == '__main__':
