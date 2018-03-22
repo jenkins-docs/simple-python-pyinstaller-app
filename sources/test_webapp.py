83a400bebaff8fa2e177fa10baf0c1c17b30c231
kudlaty <mgolaszewski@virtuslab.com>
2018-03-22 18:38:31 +0100

    feature/kjug added build stephb

---
+++ sources/test_webapp.py
@@ -0,0 +1,15 @@
+from webapp import app
+import unittest
+
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+
+if __name__ == '__main__':
+    unittest.main()
