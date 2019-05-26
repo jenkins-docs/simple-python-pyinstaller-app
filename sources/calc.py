8c907724bdd321f77f31f99351889911a9c8f84d
jaksrini <jaksrini@cisco.com>
2019-05-26 15:14:23 -0700

    hi to trigger

--- sources/calc.py
+++ sources/calc.py
@@ -6,7 +6,7 @@ NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
 '''
 
-# If 'value' is not an integer, convert it to a float and failing that, a string.
+# If 'value' is not an integer, convert it to a float and failing that, a string...
 def conv(value):
     try:
         return int(value)
