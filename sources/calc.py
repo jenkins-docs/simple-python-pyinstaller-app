affdc963b5efab36805f7b77c1ecc6197974715b
jaksrini <jaksrini@cisco.com>
2019-05-26 15:16:53 -0700

    hi to triggera

--- sources/calc.py
+++ sources/calc.py
@@ -1,5 +1,5 @@
 '''
-The 'calc' library contains the 'add2' function that takes 2 values and adds
+The 'calc' library contains the 'add2' function that takes 2 values and addsa
 them together. If either value is a string (or both of them are) 'add2' ensures
 they are both strings, thereby resulting in a concatenated result.
 NOTE: If a value submitted to the 'add2' function is a float, it must be done so
